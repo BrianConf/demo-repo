77965ccc646e0b737761d702a0dcdb30a031c927
BrianConf <88461985+BrianConf@users.noreply.github.com>
2021-08-05 00:55:57 +0200

    green shape inserted

--- main.cpp
+++ main.cpp
@@ -6,20 +6,7 @@ int main()
     RenderWindow window(VideoMode(600, 600), "SFML works!");
     CircleShape shape(100.f);
     shape.setFillColor(Color::Green);
-    Texture texture;
-    if(!texture.loadFromFile("walking2.png"));
-    Sprite sprite;
-
-    int X = texture.getSize().x;
-    int Y = texture.getSize().y;
-    int perSprite  = X/14;
-
-    IntRect rectplay(0,0,perSprite, Y);
-
-    sprite.setTexture(texture);
-    sprite.setTextureRect(rectplay);
-    sprite.setScale(0.2,0.2);
-    Clock clock;
+    
     while (window.isOpen())
     {
         Event event;
@@ -28,19 +15,8 @@ int main()
             if (event.type == Event::Closed)
                 window.close();
         }
-        if(clock.getElapsedTime().asSeconds() > 0.09){
-            if(rectplay.left >= X-X/14){
-                rectplay.left = 0;
-            }
-            else{
-                rectplay.left+=perSprite;
-            }
-            sprite.setTextureRect(rectplay);
-            clock.restart();
-        }
-
         window.clear();
-        window.draw(sprite);
+        window.draw(shape);
         window.display();
     }
 
